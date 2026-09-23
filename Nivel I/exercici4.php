122b74a1a54b7e4c39665d7a1f8605e8d4f25751
Jordi <jordi141097@gmail.com>
2026-09-23 13:44:13 +0200

    Nuevo nombre del commit

---
+++ Nivel I/exercici4.php
@@ -0,0 +1,11 @@
+<?php
+
+$miIdentidad = ["nombre" => "Jordi" , "edad" => 28 , "email" => "rger@gmail.com", "platoFavorito" => "pizza"];
+
+    echo $miIdentidad["nombre"] . "\n";
+    echo $miIdentidad["edad"] . "\n";
+    echo $miIdentidad["email"] . "\n";
+    echo $miIdentidad["platoFavorito"];
+
+
+?>
